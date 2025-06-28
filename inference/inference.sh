89358e226dc40ed000d541670fe7c2df7a1fd89d
eternity304 <harryzhou0304@gmail.com>
2025-06-28 01:17:32 -0400

    more inference script

--- inference/inference.sh
+++ inference/inference.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 
-clear
-
-export MODEL_PATH="/scratch/ondemand28/harryscz/model/CogVideoX-2b"
-export MODEL_CONFIG="/scratch/ondemand28/harryscz/diffusion/model_config.yaml"
+export MODEL_PATH=/scratch/ondemand28/harryscz/model/CogVideoX-2b
 export DATASET_PATH="/scratch/ondemand28/harryscz/head_audio/data/data256/uv"
-export CHECKPOINT_PATH="/scratch/ondemand28/harryscz/head_audio/trainOutput/checkpoint-1000.pt"
-export OUTPUT_DIR="/scratch/ondemand28/harryscz/diffusion/videoOut"
+export CHECKPOINT_PATH="/scratch/ondemand28/harryscz/head_audio/trainOutput/checkpoint-6000.pt"
 export PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True"
 export TRITON_CACHE_DIR="/scratch/ondemand28/harryscz/diffusion/trition_cache_dir"
 
-# Path to your inference script
-INFERENCE_SCRIPT="/scratch/ondemand28/harryscz/diffusion/inference/inference.py"
-
-python "$INFERENCE_SCRIPT" \
-    --model-config     "$MODEL_CONFIG" \
-    --pretrained-model "$MODEL_PATH" \
-    --checkpoint       "$CHECKPOINT_PATH" \
-    --reference-frames "$DATASET_PATH" \
-    --output           "$OUTPUT_DIR/generated.mp4" \
-    --device           "cuda" \
+python -m inference.video_generation  \
+    --model-config     inference/model_config.yaml \
+    --pretrained-model-name-or-path $MODEL_PATH \
+    --ckpt-path        $CHECKPOINT_PATH \
+    --dataset-path $DATASET_PATH \
+    --output-path      videoOut/5kStepOut.mp4 \
     --num-inference-steps  "50" \
-    --height           "256" \
-    --width            "256" \
-    --num-frames       "25" \
-    --seed             "42"
+    --sample-frames       "29" \
+    --seed             "42" \
+    --output-dir videoOut/
